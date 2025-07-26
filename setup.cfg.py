bded5600bbfee40cb981f740d4e431250688585d
Michael Krog <mic@apaq.dk>
2025-07-26 08:12:52 +0200

    Updates setup file

--- setup.cfg.py
+++ setup.cfg.py
@@ -1,2 +1,11 @@
 [metadata]
-description-file = readme.md
+name = blaubergvento_client
+version = attr: blaubergvento_client.VERSION
+author = Michael Krog
+author_email = mic@apaq.dk
+description = A Python Client for Blauberg Vento Fans (and oems like Duka S6)
+long_description = file: README.md, LICENSE
+keywords = blauberg, duka, ventilation
+license = MIT
+classifiers =
+    Programming Language :: Python :: 3
